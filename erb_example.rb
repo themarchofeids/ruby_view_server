ad5718232d857974b3bebbc8c65087b522ad9d3b
themarchofeids <jonathan.eidsvaag@gmail.com>
2015-04-10 16:12:09 -0400

    exercise additions

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 42*42
+test = ERB.new "The value of y is: <%= y %>"
+puts test.result(binding)
+
+pets = ['turbo','carmelcorn','pup','shadow','dandelion']
+testy = ERB.new "The Best Pet in the World is <%= |pet|%>
+puts testy.result(binding)
