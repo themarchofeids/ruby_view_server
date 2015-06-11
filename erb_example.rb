672eb34c38a359ecab086d62cf679ccbd6111664
themarchofeids <jonathan.eidsvaag@gmail.com>
2015-06-10 23:05:55 -0600

    homework section 1 attempt...working on section 3...some files maybe corrupted...

--- erb_example.rb
+++ erb_example.rb
@@ -1,13 +1,14 @@
 require 'erb'
 
-# x = 42
-# template = ERB.new "The value of x is: <%= x %>"
-# puts template.result(binding)
+x = 42
+template = ERB.new "The value of x is <%= x %>"
+puts template.result(binding)
 
-y = 42*42
-test = ERB.new "The value of y is: <%= y %>"
-puts test.result(binding)
+pets = ['turbo','carmelcorn','shadow']
+template2 = ERB.new "The names of my pets are: <%=pets.each do |pet|%>"
+puts template2.result(binding)
 
-pets = ['turbo','carmelcorn','pup','shadow','dandelion']
-testy = ERB.new "The Best Pet in the World is <%= |pet|%>
-puts testy.result(binding)
+erbString = "<h2><%=pets.each do |pet| 
+		puts i luv #{pet}%></h2>"
+template3 = ERB.new "The names of my pets are: <%erbString%>"
+puts template3.result(binding)
